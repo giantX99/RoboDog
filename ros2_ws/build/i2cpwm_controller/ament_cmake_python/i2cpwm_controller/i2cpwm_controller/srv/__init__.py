2754c031f42405987b3efd677a7b535b7c6344be
giantX99 <gat7hb@umsystem.edu>
2024-11-22 13:57:58 -0600

    motion_cmd debug done-1

--- ros2_ws/build/i2cpwm_controller/ament_cmake_python/i2cpwm_controller/i2cpwm_controller/srv/__init__.py
+++
@@ -1,4 +0,0 @@
-from i2cpwm_controller.srv._drive_mode import DriveMode  # noqa: F401
-from i2cpwm_controller.srv._int_value import IntValue  # noqa: F401
-from i2cpwm_controller.srv._servos_config import ServosConfig  # noqa: F401
-from i2cpwm_controller.srv._stop_servos import StopServos  # noqa: F401
